50162a890a6c7a6d6dad314763032e8efa96a07a
ThomdabeasT <thom.hemenway@wsu.edu>
2016-10-31 16:01:15 -0700

    added icons and fixed 'moving up' bug

--- season.js
+++ season.js
@@ -1,14 +1,41 @@
 var amount = Math.floor(window.innerWidth / (100));
 var snowball, snowballWidth = 0;
 var auto;
+var snowflakes = [];
 
 function startAuto() {
-	    auto = window.setInterval(continuousFall, 20 * 1000);
-		console.log("Regained focus");
+	// Create snowflakes
+	for (var i = amount - 1; i >= 0; i--) {
+		var img = document.createElement("img");
+		img.id = "snowflake" + i;
+		img.style.position = "absolute";
+		img.style.top = "-32px";
+		img.style.left = (i * 100) + "px";
+		img.style.width = "32px";
+		img.style.height = "32px";
+		img.style.zIndex = "1000";
+		img.src = chrome.extension.getURL("snowflake.png");
+		
+		img.addEventListener("mouseover", function() {
+			snowball.style.width = (snowballWidth += 2) + "px";
+		});
+
+
+		document.body.appendChild(img);
+		snowflakes.push(img);
+	}
+	
+	auto = window.setInterval(continuousFall, 20 * 1000);
+	console.log("Regained focus");
 }
 function stopAuto() {
-	for (var j = amount - 1; j >= 0; j--) {
-		$("#snowflake" + j).stop(true, true);
+	for (var j = snowflakes.length - 1; j >= 0; j--) {
+		$(snowflakes[j]).stop(true, true);
+		
+	}
+	
+	while(snowflakes.length > 0){
+		document.body.removeChild(snowflakes.pop());
 	}
 	
 	console.log("lost focus");
@@ -54,40 +81,20 @@ $(function() {
 	snowball.style.zIndex = "1000";
 	document.body.appendChild(snowball);
 
-	// Create snowflakes
-	for (var i = amount - 1; i >= 0; i--) {
-		var img = document.createElement("img");
-		img.id = "snowflake" + i;
-		img.style.position = "absolute";
-		img.style.top = "-32px";
-		img.style.left = (i * 100) + "px";
-		img.style.width = "32px";
-		img.style.height = "32px";
-		img.style.zIndex = "1000";
-		img.src = chrome.extension.getURL("snowflake.png");
-		
-		img.addEventListener("mouseover", function() {
-			snowball.style.width = (snowballWidth += 2) + "px";
-		});
-
-
-		document.body.appendChild(img);
-	}
-
-	continuousFall(); // Make it start as soon as the page loads
 	startAuto();
+	continuousFall(); // Make it start as soon as the page loads
 });
 
 function continuousFall() {
-	for (var j = amount - 1; j >= 0; j--) {
+	for (var j = snowflakes.length - 1; j >= 0; j--) {
 		if (j % 3 == 0) {
-			snowAnimation($("#snowflake" + j), j, 1000, 9000);
+			snowAnimation($(snowflakes[j]), j, 1000, 9000);
 		}
 		else if (j % 3 == 1) {
-			snowAnimation($("#snowflake" + j), j, 3000, 9000);
+			snowAnimation($(snowflakes[j]), j, 3000, 9000);
 		}
 		else{
-			snowAnimation($("#snowflake" + j), j, 5000, 9000);
+			snowAnimation($(snowflakes[j]), j, 5000, 9000);
 		}
 	}
 }
